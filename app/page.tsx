a26d0593375096bb5841e2006197394831236f45
mdaminul1024 <mdfahad1024@gmail.com>
2025-03-18 17:36:50 +0600

    header section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import AddTodo from "@/Components/add-todo";
+import { LuClipboardList } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Md Aminul Islam</h2>
+    <div className="py-16 max-w-[900px] mx-auto">
+      <h2 className="text-3xl font-bold text-slate-800 flex justify-center gap-5 items-center"><LuClipboardList className="text-4xl text-green-500"/> TODO NEXT + TYPESCRIPT <LuClipboardList className="text-4xl text-green-500"/></h2>
+      <AddTodo />
     </div>
   );
 }
